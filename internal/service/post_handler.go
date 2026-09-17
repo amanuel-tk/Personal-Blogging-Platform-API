98d48765b1d8dd5a5e34b3572a5db34d40a303ad
Amanuel <amiiko1927@gmail.com>
2026-09-17 23:23:54 +0300

    post service done

--- internal/service/post_handler.go
+++ internal/service/post_handler.go
@@ -1,1 +1,32 @@
 package service
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/amanuel-tk/Personal-Blogging-Platform-API/internal/model"
+)
+
+type PostRepository struct {
+	db *sql.DB
+}
+
+func NewPostRepository(db *sql.DB) *PostRepository {
+	return &PostRepository{
+		db: db,
+	}
+}
+
+func (r *PostRepository) CreatePost(ctx context.Context, post model.Post) (*model.Post, error) {
+
+	query := `INSERT INTO post (title,content) VALUE ($1,$2) RETURNING id`
+
+	err := r.db.QueryRowContext(ctx, query, post.Title, post.Content).Scan(&post.ID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &post, nil
+
+}
