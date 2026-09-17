0eade651cc37bebaf472cc25a9d8b5eadd58fce5
Amanuel <amiiko1927@gmail.com>
2026-09-17 23:38:43 +0300

    done with making the hadler

--- internal/handler/post_handler.go
+++ internal/handler/post_handler.go
@@ -1,1 +1,41 @@
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/amanuel-tk/Personal-Blogging-Platform-API/internal/model"
+	"github.com/amanuel-tk/Personal-Blogging-Platform-API/internal/service"
+)
+
+type PostHandler struct {
+	service *service.PostRepository
+}
+
+func NewPostHandler(service *service.PostRepository) *PostHandler {
+	return &PostHandler{
+		service: service,
+	}
+}
+
+func (h *PostHandler) CreatePost(w http.ResponseWriter, r http.Request) {
+	var post model.Post
+
+	err := json.NewDecoder(r.Body).Decode(&post)
+	if err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	createdPost, err := h.service.CreatePost(r.Context(), post)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(createdPost)
+
+}
