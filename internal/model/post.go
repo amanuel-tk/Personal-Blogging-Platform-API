b03f72dd77a4919f8ae36d1615c28886101c1dc1
Amanuel <amiiko1927@gmail.com>
2026-09-17 23:11:11 +0300

    created the post

--- internal/model/post.go
+++ internal/model/post.go
@@ -1,1 +1,10 @@
 package model
+
+type Post struct {
+	ID        string `json:"id"`
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	Tags      string `json:"tags"`
+	CreatedAt string `json:"createdAt"`
+	UpdatedAt string `json:"updatedAt"`
+}
