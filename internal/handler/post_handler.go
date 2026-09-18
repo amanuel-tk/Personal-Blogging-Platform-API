a40ddb688ed2a9d4067ace52672ff4be1429740e
Amanuel <amiiko1927@gmail.com>
2026-09-19 00:38:36 +0300

    done making the hadler for update post

--- internal/handler/post_handler.go
+++ internal/handler/post_handler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/amanuel-tk/Personal-Blogging-Platform-API/internal/model"
 	"github.com/amanuel-tk/Personal-Blogging-Platform-API/internal/service"
@@ -25,7 +26,10 @@ func (h *PostHandler) CreatePost(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&post)
 	if err != nil {
-		http.Error(w, "invalid request body", http.StatusBadRequest)
+
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "invalid request body"})
+
 		return
 	}
 
@@ -35,7 +39,9 @@ func (h *PostHandler) CreatePost(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		log.Printf("failed to create post :%v", err)
-		http.Error(w, "failed to create post", http.StatusBadRequest)
+
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "failed to create post"})
 		return
 	}
 
@@ -45,16 +51,48 @@ func (h *PostHandler) CreatePost(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (h PostHandler) GetPosts(w http.ResponseWriter, r *http.Request) {
+func (h *PostHandler) GetPosts(w http.ResponseWriter, r *http.Request) {
 	getPosts, err := h.service.GetPost(r.Context())
 
 	if err != nil {
 		log.Printf("failed to fetch posts: %v", err)
-		http.Error(w, "failed to get posts", http.StatusBadRequest)
+
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "failed to fetch posts"})
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(getPosts)
+}
+
+func (h *PostHandler) UpdatePost(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	id = strings.TrimSpace(id)
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "id is required"})
+		return
+	}
+
+	var req model.Post
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "id is required"})
+		return
+	}
+
+	updatePost, err := h.service.UpdatePost(r.Context(), id, req)
+
+	if err != nil {
+		log.Printf("failed to update post:%v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "failed to update post"})
+	}
+
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(updatePost)
 }
