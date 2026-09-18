1f0c9ba066bf673b15372bb93b0660e2a93de1ab
Amanuel <amiiko1927@gmail.com>
2026-09-18 23:54:56 +0300

    done with validation

--- internal/service/post_service.go
+++ internal/service/post_service.go
@@ -3,36 +3,67 @@ package service
 import (
 	"context"
 	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/amanuel-tk/Personal-Blogging-Platform-API/internal/model"
 	"github.com/amanuel-tk/Personal-Blogging-Platform-API/internal/repository"
+	"github.com/go-playground/validator/v10"
 )
 
 type PostService struct {
-	repo *repository.PostRepository
+	repo      *repository.PostRepository
+	validator *validator.Validate
 }
 
 func NewPostService(repo *repository.PostRepository) *PostService {
 	return &PostService{
-		repo: repo,
+		repo:      repo,
+		validator: validator.New(),
 	}
 }
 
 func (s *PostService) CreatePost(ctx context.Context, post model.Post) (*model.Post, error) {
 
-	post.Title = strings.TrimSpace(post.Title)
-	post.Content = strings.TrimSpace(post.Content)
+	// post.Title = strings.TrimSpace(post.Title)
+	// post.Content = strings.TrimSpace(post.Content)
 
-	if post.Title == "" {
-		return nil, errors.New("title is required")
+	// if post.Title == "" {
+	// 	return nil, errors.New("title is required")
+	// }
+	// if post.Content == "" {
+	// 	return nil, errors.New("content is required")
+	// }
+
+	fmt.Println(post.Content)
+
+	if err := s.validator.Struct(post); err != nil {
+		return nil, err
 	}
-	if post.Content == "" {
-		return nil, errors.New("content is required")
-	}
+
 	return s.repo.CreatePost(ctx, post)
 }
 
 func (s *PostService) GetPost(ctx context.Context) ([]model.Post, error) {
 	return s.repo.GetPost(ctx)
+}
+
+func (s *PostService) UpdatePost(ctx context.Context, id string, updatedPost model.Post) (*model.Post, error) {
+
+	id = strings.TrimSpace(id)
+
+	updatedPost.Content = strings.TrimSpace(updatedPost.Content)
+	updatedPost.Title = strings.TrimSpace(updatedPost.Title)
+	updatedPost.Tags = strings.TrimSpace(updatedPost.Tags)
+
+	if id == "" {
+		return nil, errors.New("id is required")
+	}
+
+	if updatedPost.Title == "" || updatedPost.Content == "" || updatedPost.Tags == "" {
+		return nil, errors.New("title, content and tags are required.")
+	}
+
+	return s.repo.UpdatePost(ctx, id, updatedPost)
+
 }
