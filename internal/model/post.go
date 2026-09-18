1b0e0c9d302765c06f95f07a01563411cd60eaca
Amanuel <amiiko1927@gmail.com>
2026-09-18 23:32:00 +0300

    added a validator

--- internal/model/post.go
+++ internal/model/post.go
@@ -2,9 +2,9 @@ package model
 
 type Post struct {
 	ID        string `json:"id"`
-	Title     string `json:"title"`
-	Content   string `json:"content"`
-	Tags      string `json:"tags"`
+	Title     string `json:"title" validate:"required,min=3,max=100"`
+	Content   string `json:"content" validate:"required,min3,max=200"`
+	Tags      string `json:"tags" validate:"required"`
 	CreatedAt string `json:"createdAt"`
 	UpdatedAt string `json:"updatedAt"`
 }
